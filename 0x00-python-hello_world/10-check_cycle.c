06f98f9481fc2b2a96408d3979a4b846f44b8f17
hb4y <hugobaymon@gmail.com>
2019-12-09 22:11:08 +0000

    check cycles in single linked list

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * check_cycle - Check if a cycle exist in a linked list
+ * @list: Single linked list to check for cycles
+ * Return: 0 if no cycle, 1 if a cycle is found
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *first = NULL;
+	listint_t *second = NULL;
+
+	if (list)
+	{
+		first = list;
+		second = list;
+
+		do {
+			first = first->next;
+			second = second->next->next;
+
+			if (first == second)
+				return (1);
+
+		} while (first->next && second->next->next);
+	}
+	return (0);
+}
