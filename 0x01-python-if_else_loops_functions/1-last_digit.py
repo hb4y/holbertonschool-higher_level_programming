64698663d28cef643b23267cb762e926181dd80f
hb4y <hugobaymon@gmail.com>
2019-12-10 17:57:52 +0000

    re order conditions

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,15 +3,14 @@ import random
 
 number = random.randint(-10000, 10000)
 last = int(repr(number)[-1])
-
 if (number < 0):
     last *= -1
 
 str1 = "Last digit of {:d} is {:d} and is ".format(number, last)
 
-if (last == 0):
-    print(str1 + "0")
-elif (last < 6):
-    print(str1 + "less than 6 and not 0")
-elif (last > 5):
+if (last > 5):
     print(str1 + "greater than 5")
+elif (last == 0):
+    print(str1 + "0")
+else:
+    print(str1 + "less than 6 and not 0")
