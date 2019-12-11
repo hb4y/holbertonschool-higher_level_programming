5d01b3f67f74f9b1bb8831e483d843f4a8d6b32a
hb4y <hugobaymon@gmail.com>
2019-12-11 01:16:38 +0000

    insert node in linked list - pending style

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,46 @@
+#include "lists.h"
+
+/**
+ * insert_node - Insert a new node in a linked list
+ * @head: head of the list
+ * @number: the number for the new node
+ * Return: New node on sucess or NULL if fail
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new;
+	listint_t *tmp;
+
+	new = malloc(sizeof(listint_t));
+
+	if (!new)
+		return(NULL);
+
+	new->n = number;
+
+	if (!(*head))
+	{
+		new->next = NULL;
+		*head = new;
+		return(new);
+	}
+
+	for (tmp = *head; tmp; tmp = tmp->next)
+	{
+		if (!(tmp->next))
+		{
+			new->next = NULL;
+			tmp->next = new;
+			return(new);
+		}
+		if (tmp->next->n >= number)
+		{
+			new->next = tmp->next;
+			tmp->next = new;
+			return (new);
+		}
+	}
+
+	free(new);
+	return (NULL);
+}
