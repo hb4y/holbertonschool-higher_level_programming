29d6422e62cb889a2f707e7f86391cfe9f89c901
hb4y <hugobaymon@gmail.com>
2019-12-12 22:40:20 +0000

    fix 1 argument output

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,10 +4,11 @@ if __name__ == "__main__":
 
     if len(sys.argv) < 2:
         print("0 arguments.")
+        exit()
     elif len(sys.argv) == 2:
         print("1 argument:")
     else:
         print("{:d} arguments:".format(len(sys.argv) - 1))
 
-        for index, data in enumerate(sys.argv[1:]):
-                print("{:d}: {}".format(index + 1, data))
+    for index, data in enumerate(sys.argv[1:]):
+        print("{:d}: {}".format(index + 1, data))
