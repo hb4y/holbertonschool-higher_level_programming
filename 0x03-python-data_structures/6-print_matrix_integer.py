c3e17eb9b937223643c6f3ae5a3addfb9a9a824c
hb4y <hugobaymon@gmail.com>
2019-12-17 17:31:29 +0000

    fix - no list

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix is not None:
+    if not (len(matrix) == 1 and len(matrix[0]) == 0):
         for i in matrix:
             for index, elem in enumerate(i):
                 if (index + 1) != len(i):
                     print("{:d} ".format(elem), end='')
                 else:
                     print("{:d}".format(elem))
+    else:
+        print("")
