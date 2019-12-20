8d7b843cb9cb581f4781e08ab6f466fbd1991d26
hb4y <hugobaymon@gmail.com>
2019-12-20 05:06:11 +0000

    mucha loca yo... disque or

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if (isinstance(roman_string, str)) or (roman_string):
+    if (isinstance(roman_string, str)) and (roman_string):
         d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         res = 0
 
-        for i in range(0, len(roman_string)):
+        for i in range(len(roman_string)):
             if (i > 0) and (d[roman_string[i - 1]] < d[roman_string[i]]):
                 res += d[roman_string[i]] - 2 * d[roman_string[i - 1]]
             else:
