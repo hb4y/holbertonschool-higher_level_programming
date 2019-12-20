3476eae093171b97786b3dff1e243e902c90737b
hb4y <hugobaymon@gmail.com>
2019-12-20 04:45:07 +0000

    change conditional

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if (not isinstance(roman_string, str)) or (roman_string is None):
+    if (isinstance(roman_string, str)) or (roman_string):
         d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         res = d[roman_string[0]]
 
