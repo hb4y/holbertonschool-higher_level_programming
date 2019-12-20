0c82ac4c1f0c4e1c0b13cb1b050ffa05845e97f8
hb4y <hugobaymon@gmail.com>
2019-12-20 04:42:57 +0000

    dont do anything if is not str or is Nona

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    roman_string = roman_string.upper()
-    d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    res = 0
-
     if (not isinstance(roman_string, str)) or (roman_string is None):
-        return res
+        d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        res = d[roman_string[0]]
 
-    if len(roman_string) == 1:
-            return d[roman_string]
+        if len(roman_string) == 1:
+                return int(res)
 
-    res = d[roman_string[0]]
-    for i in range(1, len(roman_string)):
-        if d[roman_string[i - 1]] < d[roman_string[i]]:
-            res += d[roman_string[i]] - 2 * d[roman_string[i - 1]]
-        else:
-            res += d[roman_string[i]]
-    return int(res)
+        for i in range(1, len(roman_string)):
+            if d[roman_string[i - 1]] < d[roman_string[i]]:
+                res += d[roman_string[i]] - 2 * d[roman_string[i - 1]]
+            else:
+                res += d[roman_string[i]]
+        return int(res)
+    else:
+        return 0
