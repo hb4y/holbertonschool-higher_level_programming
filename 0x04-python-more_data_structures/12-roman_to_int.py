35529fda9af669d30c04c3fd6da1b30ad2f1ac6f
hb4y <hugobaymon@gmail.com>
2019-12-20 04:14:10 +0000

    check if string or None

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,6 +4,9 @@ def roman_to_int(roman_string):
     d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     res = 0
 
+    if not isinstance(roman_string, str) or roman_string is None:
+        return res
+
     for i in roman_string:
         if not (i in d.keys()):
             return res
