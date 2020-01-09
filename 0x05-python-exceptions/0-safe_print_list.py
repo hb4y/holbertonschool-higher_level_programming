03831acdd40449b0c052aebc4d24751984adf3e3
hb4y <hugobaymon@gmail.com>
2020-01-09 00:31:34 +0000

    0 task

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    nb = 0
+    try:
+        for i in range(x):
+            print("{}".format(my_list[i]), end="")
+            nb = nb + 1
+    except:
+        print("")
+        return nb
+    print("")
+    return nb
