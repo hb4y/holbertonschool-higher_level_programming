3496322a38b4c25b79f67988236e4f75e972ce9f
hb4y <hugobaymon@gmail.com>
2020-01-10 03:09:32 +0000

    down offset

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -24,6 +24,8 @@ class Square:
         if self.__size == 0:
             print("")
             return
+        for offset in range(self.__position[1]):
+            print("")
         for i in range(self.__size):
             for offset in range(self.__position[0]):
                 print(" ", end="")
