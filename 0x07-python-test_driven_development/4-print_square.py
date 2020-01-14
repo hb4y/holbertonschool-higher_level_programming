769e54237832fe504f37ce18699f57ebd4c29af1
hb4y <hugobaymon@gmail.com>
2020-01-14 01:25:21 +0000

    task 4... no test

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,13 @@
+def print_square(size):
+    if not (isinstance(size, int)):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    if size == 0:
+        print("")
+        return
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print("")
