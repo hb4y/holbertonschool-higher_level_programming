bca37e0d9334e84bb8e49020de089c5de1a11f6c
hb4y <hugobaymon@gmail.com>
2020-01-22 23:45:28 +0000

    only inherit

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -7,4 +7,4 @@ the object is an instance of a class that inherited
 
 def inherits_from(obj, a_class):
     """return True or False"""
-    return issubclass(type(obj), a_class)
+    return issubclass(type(obj), a_class) and (not type(obj) is a_class)
