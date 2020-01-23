86c25e7d85ee49f21c2f553e13fd537959ccc793
hb4y <hugobaymon@gmail.com>
2020-01-23 04:16:13 +0000

    else

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -8,4 +8,5 @@ def add_attribute(obj, aname, value):
     """if not inmutable set attrib"""
     if isinstance(obj, (bool, int, float, tuple, str, frozenset)):
         raise TypeError("can't add new attribute")
-    setattr(obj, aname, value)
+    else:
+        setattr(obj, aname, value)
