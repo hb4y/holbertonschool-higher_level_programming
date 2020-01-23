d05e8978d335cdb07a30182b37db228059fe65f5
hb4y <hugobaymon@gmail.com>
2020-01-23 02:51:53 +0000

    task 11

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""
+class Square
+"""
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    def __str__(self):
+        msg = "[Square] " + str(self.__size) + "/" + str(self.__size)
+        return msg
