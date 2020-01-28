135cb46b63e9dae0cae26fd64ca75c8cf5db3f13
hb4y <hugobaymon@gmail.com>
2020-01-28 04:24:11 +0000

    to file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -25,3 +25,14 @@ class Base:
         if list_dictionaries and len(list_dictionaries):
             return json.dumps(list_dictionaries)
         return "[]"
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        tmp = []
+        if list_objs:
+            for items in list_objs:
+                tmp.append(items.to_dictionary())
+
+        info = cls.to_json_string(tmp)
+        with open(str(cls.__name__) + ".json", "w") as new_file:
+            new_file.write(info)
