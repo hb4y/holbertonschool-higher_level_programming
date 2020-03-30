f706566e1b731df4af2d2901b82e1d869ffe82ae
hb4y <hugobaymon@gmail.com>
2020-03-30 01:01:10 +0000

    [task 2] Vulnerable stmt, format implemented

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+"""
+ displays all values in the states where name matches the argument
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) < 4:
+        print("Usage: {} username password database_name".format(sys.argv[0]))
+        exit(1)
+
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3])
+
+    cur = conn.cursor()
+    stmt = "SELECT * \
+            FROM states \
+            WHERE BINARY name = '{}' \
+            ORDER BY id ASC".format(sys.argv[4])
+
+    cur.execute(stmt)
+    query = cur.fetchall()
+    for row in query:
+        print(row)
+
+    cur.close()
+    conn.close()
