633bd2f91bdc5ce9630ba8838e36f66fee775d01
hb4y <hugobaymon@gmail.com>
2020-03-30 01:42:22 +0000

    [task 4] script that lists all cities

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+"""
+ displays all cities
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) < 4:
+        print("Usage: {} username password database_name".format(sys.argv[0]))
+        exit(1)
+
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3])
+
+    cur = conn.cursor()
+    cur.execute("SELECT c.id, c.name, s.name \
+                 FROM states s, cities c \
+                 WHERE s.id = c.state_id \
+                 ORDER BY id ASC")
+
+    query = cur.fetchall()
+    for row in query:
+        print(row)
+
+    cur.close()
+    conn.close()
