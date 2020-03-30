3d7be0bf45bf4c9f06be9359451d025ad9754513
hb4y <hugobaymon@gmail.com>
2020-03-30 03:52:05 +0000

    [task 11] adds the State object Louisiana

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""
+Adds the State object "Louisiana"
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
+
+
+if __name__ == "__main__":
+    cs = 'mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1],
+                                                     sys.argv[2], sys.argv[3])
+    engine = create_engine(cs, pool_pre_ping=True)
+    s = Session(engine)
+    new_state = State(name='Louisiana')
+    s.add(new_state)
+    s.commit()
+    state = s.query(State).filter_by(name='Louisiana').first()
+    print("{}".format(state.id))
+    s.close()
