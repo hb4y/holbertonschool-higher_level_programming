7e1540ca15fa2c7cf34888d492e021eb5b3cc0d1
hb4y <hugobaymon@gmail.com>
2020-03-30 00:40:02 +0000

    [task 0] Create table and list all states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+lists all states from the database hbtn_0e_0_usa
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) < 4:
+        print("Usage: {} username password database_name".format(sys.argv[0]))
+        exit(1)
+
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3])
+
+    cur = conn.cursor()
+    cur.execute("SELECT * \
+                 FROM states \
+                 ORDER BY id ASC")
+    query = cur.fetchall()
+    for row in query:
+        print(row)
+
+    cur.close()
+    conn.close()
