4b44bf6846e8aef4ef29dc139976f126e9a0818d
hb4y <hugobaymon@gmail.com>
2020-03-30 02:51:10 +0000

    [task 6] State class represents a table

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""
+State class
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    State class represents a table.
+    """
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
