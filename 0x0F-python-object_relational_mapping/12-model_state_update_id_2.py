a06a2c33e059f99dd3e73513f6292511c67e30ea
hb4y <hugobaymon@gmail.com>
2020-03-30 04:00:33 +0000

    [task 12] changes the name of a State

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""
+Change the name of a State
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
+
+
+if __name__ == "__main__":
+    cs = 'mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1],
+                                                     sys.argv[2], sys.argv[3])
+    engine = create_engine(cs, pool_pre_ping=True)
+    s = Session(engine)
+    state = s.query(State).filter_by(id=2).first()
+    state.name = "New Mexico"
+    s.commit()
+    s.close()
