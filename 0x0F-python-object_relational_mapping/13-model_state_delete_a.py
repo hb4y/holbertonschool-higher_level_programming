b157c5813838977cfe6d99692b8d5c4273911c01
hb4y <hugobaymon@gmail.com>
2020-03-30 04:09:38 +0000

    [task 13] deletes all State objects with a name containing the letter a

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""
+deletes all State objects with a letter
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
+
+
+if __name__ == "__main__":
+    cs = 'mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1],
+                                                     sys.argv[2], sys.argv[3])
+    engine = create_engine(cs, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    s = Session(engine)
+    for state in s.query(State).filter(State.name.contains('a')).all():
+        s.delete(state)
+    s.commit()
+    s.close()
