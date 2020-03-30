5479fc1c124c63bcf6cc32c792fe99526ecadc89
hb4y <hugobaymon@gmail.com>
2020-03-30 02:59:30 +0000

    [task 7] lists all State objects from the database

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""
+ lists all State objects from the database
+"""
+import sys
+import warnings
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
+
+
+if __name__ == "__main__":
+    warnings.simplefilter('ignore', Warning)
+    cs = 'mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1],
+                                                     sys.argv[2], sys.argv[3])
+    engine = create_engine(cs, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+
+    for state in session.query(State).order_by(State.id).all():
+        print("{}: {}".format(state.id, state.name))
+
+    session.close()
+    warnings.simplefilter('always', Warning)
