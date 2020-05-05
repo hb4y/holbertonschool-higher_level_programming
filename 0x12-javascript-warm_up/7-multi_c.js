7369abfd4d3810834ce843c11d329c599d5f578c
hb4y <hugobaymon@gmail.com>
2020-05-05 23:38:32 +0000

    [task 7] args + loop

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+
+const process = require('process');
+
+const args = process.argv;
+let number = parseInt(args[2]);
+
+if (number) {
+  for (; number > 0; number--) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
