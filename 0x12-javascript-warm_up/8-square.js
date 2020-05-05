5bfa5defbdc6064cad4732be2fc1a4eaf152677e
hb4y <hugobaymon@gmail.com>
2020-05-05 23:50:13 +0000

    [task 8] print square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+
+const process = require('process');
+
+const args = process.argv;
+const number = parseInt(args[2]);
+
+if (number) {
+  for (let i = number; i > 0; i--) {
+    console.log('X'.repeat(number));
+  }
+} else {
+  console.log('Missing size');
+}
