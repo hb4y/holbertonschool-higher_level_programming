1829216522cade6b073896c0a6e341ac5bc975a2
hb4y <hugobaymon@gmail.com>
2020-05-05 23:28:13 +0000

    [task 5] Is number or not

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+
+const process = require('process');
+
+const args = process.argv;
+const number = parseInt(args[2]);
+
+if (number) {
+  console.log('My number: ' + number);
+} else {
+  console.log('Not a number');
+}
