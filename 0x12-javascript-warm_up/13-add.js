595bf4ccc7139c51a1808e5a5dcfe9d963bbd837
hb4y <hugobaymon@gmail.com>
2020-05-06 03:21:13 +0000

    [task 13] export function add

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+
+exports.add = function (a, b) {
+  const first = parseInt(a);
+  const second = parseInt(b);
+
+  if (first && second) {
+    return (first + second);
+  } else {
+    return (NaN);
+  }
+};
