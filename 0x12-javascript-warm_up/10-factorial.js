a5db0fe2d4716c8b4ab081b953f054abdfcc9987
hb4y <hugobaymon@gmail.com>
2020-05-06 00:12:47 +0000

    [task 10] factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+
+const process = require('process');
+
+const args = process.argv;
+const number = parseInt(args[2]);
+
+function factorial (number) {
+  if (number === 0) {
+    return (1);
+  } else {
+    return (number * factorial(number - 1));
+  }
+}
+
+if (number) {
+  console.log(factorial(number));
+} else {
+  console.log(1);
+}
