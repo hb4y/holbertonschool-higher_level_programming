e00ec90be10d24f47895155068bc583d2492629d
hb4y <hugobaymon@gmail.com>
2020-05-06 00:02:06 +0000

    [task 9] add two numbers

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+
+const process = require('process');
+
+const args = process.argv;
+const first = parseInt(args[2]);
+const second = parseInt(args[3]);
+
+if (first && second) {
+  console.log(first + second);
+} else {
+  console.log(NaN);
+}
