75bb6576ab9ea96850b1af07d19b3a7918469868
hb4y <hugobaymon@gmail.com>
2020-05-06 00:51:54 +0000

    [task 11] second min value

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,26 @@
 #!/usr/bin/node
+
+const process = require('process');
+
+const args = process.argv;
+const len = args.length;
+let first = -Infinity;
+let second = -Infinity;
+let number;
+
+if (len <= 2) {
+  console.log(0);
+} else if (len === 3) {
+  console.log(0);
+} else {
+  for (let i = 2; i < len; i++) {
+    number = parseInt(args[i]);
+    if (number > first) {
+      second = first;
+      first = number;
+    } else if (number > second) {
+      second = number;
+    }
+  }
+  console.log(second);
+}
